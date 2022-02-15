c53185a90ef6e61ede7ef4f3e5fbfea26d309e0f
jodufour <jodufour@student.42.fr>
2022-02-15 04:40:05 +0000

    Fix ex04 [OK]

--- ex04/srcs/main.cpp
+++ ex04/srcs/main.cpp
@@ -6,13 +6,14 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 08:54:16 by jodufour          #+#    #+#             */
-/*   Updated: 2022/01/05 12:50:06 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/02/15 04:37:31 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 static int	__usage_err(void)
 {
@@ -32,72 +33,44 @@ static int	__open_err(void)
 	return EXIT_FAILURE;
 }
 
-static int	__read_err(void)
+static std::string	__sed(std::string content, std::string const &oldPattern,
+	std::string const &newPattern)
 {
-	std::cerr
-	<< "Error: cannot read file"
-	<< std::endl;
-	return EXIT_FAILURE;
-}
-
-static void	__sed(
-	std::ifstream &ifs,
-	std::ofstream &ofs,
-	std::string const oldPattern,
-	std::string const newPattern)
-{
-	std::string	content(
-		(std::istreambuf_iterator<char>(ifs)),
-		(std::istreambuf_iterator<char>()));
 	std::size_t	i;
-	std::size_t	j;
 
-	i = 0;
-	j = content.find(oldPattern.c_str(), i, oldPattern.length());
-	while (j != content.npos)
+	i = content.find(oldPattern.c_str(), 0, oldPattern.length());
+	while (i != std::string::npos)
 	{
-		content.erase(j, oldPattern.length());
-		content.insert(j, newPattern, 0, newPattern.length());
-		i += j + newPattern.length();
-		j = content.find(oldPattern.c_str(), i, oldPattern.length());
+		content.erase(i, oldPattern.length());
+		content.insert(i, newPattern);
+		i = content.find(oldPattern.c_str(), i + newPattern.length(),
+			oldPattern.length());
 	}
-	ofs << content;
+	return content;
 }
-
-static int	__close_err(void)
-{
-	std::cerr
-	<< "Error: cannot close file"
-	<< std::endl;
-	return EXIT_FAILURE;
-}
-
 
 int	main(int const ac, char const **av)
-{
-	std::string		filename;
-	std::ifstream	ifs;
-	std::ofstream	ofs;
+{	
+	std::string			filename;
+	std::ifstream		ifs;
+	std::ofstream		ofs;
+	std::stringstream	buff;
 
-	if (ac != 4)
+	if (ac != 4 || !av[2][0] || !av[3][0])
 		return __usage_err();
 	filename = av[1];
 	ifs.open(filename.c_str());
 	if (ifs.fail())
 		return __open_err();
-	ifs.get();
-	if (ifs.fail())
-		return __read_err();
-	ifs.seekg(ifs.beg);
 	ofs.open(filename.append(".replace").c_str());
 	if (ofs.fail())
 	{
 		ifs.close();
 		return __open_err();
 	}
-	__sed(ifs, ofs, av[2], av[3]);
+	buff << ifs.rdbuf();
+	ofs << __sed(buff.str(), std::string(av[2]), std::string(av[3]));
 	ifs.close();
-	if (ifs.fail())
-		return __close_err();
+	ofs.close();
 	return EXIT_SUCCESS;
 }
